dce2f165915de612f41b9f51ad0ecad6182af19f
John Alexander Rios Guisao <john@users-MacBook-Pro.local>
2023-08-15 22:26:35 -0500

    Atoi con error

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"fmt"
 
-	"github.com/johnrios07/godesde0/variables"
+	"github.com/johnrios07/godesde0/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConviertoATexto(1588)
+	/*estado, texto := variables.ConviertoATexto(1588)
 	fmt.Println(estado)
+	fmt.Println(texto)
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Esto no  es windows")
+	} else {
+		fmt.Println("Esto es", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "Linux.":
+		fmt.Println("Esto es Linux")
+	case "windows":
+		fmt.Println("Esto es windows")
+	default:
+		fmt.Printf("%s \n", os)
+	}*/
+
+	numero, texto := ejercicios.ConvNumerico("cdcd")
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
